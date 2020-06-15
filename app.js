f3fe3ff11743b6b44a4e8e24bc13ff1ac54d807a
Ludo <ludoloops@protonmail.com>
2020-06-15 17:11:11 +0200

    feat: 100 adding a product model, use Object as a model, instead of variable in constroller

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const page404 = require('./routes/page404');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
