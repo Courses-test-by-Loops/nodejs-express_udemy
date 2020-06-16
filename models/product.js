7070214ac78238787f4ed55bfe0c2c7069b923bb
Ludo <ludoloops@protonmail.com>
2020-06-16 13:40:46 +0200

    feat: 109 registering the routes

--- models/product.js
+++ models/product.js
@@ -1,34 +1,37 @@
 const fs = require('fs');
 const path = require('path');
+
 const p = path.join(
   path.dirname(process.mainModule.filename),
   'data',
   'products.json'
 );
-const getProductFromFile = (cb) => {
 
+const getProductsFromFile = (cb) => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
-      return cb([]);
+      cb([]);
     } else {
       cb(JSON.parse(fileContent));
     }
   });
 };
+
 module.exports = class Product {
   constructor(t) {
     this.title = t;
   }
+
   save() {
-    getProductFromFile((products) => {
+    getProductsFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
-        console.error(err);
+        console.log(err);
       });
     });
   }
 
   static fetchAll(cb) {
-    getProductFromFile(cb);
+    getProductsFromFile(cb);
   }
 };
