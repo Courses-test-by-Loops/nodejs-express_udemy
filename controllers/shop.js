0a6f78ab21e9034c1e8cbfd4cc597694b66a1e4a
Ludo Loops <ludoloops@prontonmail.com>
2020-06-28 16:35:59 +0200

    feat: 121 passing Data with POST

--- controllers/shop.js
+++ controllers/shop.js
@@ -12,8 +12,13 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  console.log(prodId);
-  res.redirect('/');
+  Product.findById(prodId, product => {
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products',
+    });
+  });
 };
 
 exports.getIndex = (req, res, next) => {
@@ -33,6 +38,12 @@ exports.getCart = (req, res, next) => {
   });
 };
 
+exports.postCart = (req, res, next) => {
+  const productId = req.body.productId;
+  console.log(productId);
+
+  res.redirect('/cart');
+};
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
     path: '/orders',
