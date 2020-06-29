fab98cd70dcc7d0cb48e3d81aa3a43745bddc148
Ludo Loops <ludoloops@prontonmail.com>
2020-06-29 19:53:20 +0200

    feat: 145 insert data into db

--- models/product.js
+++ models/product.js
@@ -10,7 +10,14 @@ module.exports = class Product {
     this.price = price;
   }
 
-  save() {}
+  save() {
+    return db.execute('INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?);', [
+      this.title,
+      this.price,
+      this.imageUrl,
+      this.description,
+    ]);
+  }
   static deleteById(id) {}
   static fetchAll() {
     return db.execute('SELECT * FROM products');
