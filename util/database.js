959acc66927f43f9408e8f4d4bae75150556f321
Ludo Loops <ludoloops@prontonmail.com>
2020-06-30 01:32:53 +0200

    feat 151: sequelize define mode

--- util/database.js
+++ util/database.js
@@ -1,9 +1,5 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  database: 'node-complete',
-  password: 'nodecomplete',
-});
-module.exports = pool.promise();
+const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', { dialect: 'mysql', host: 'localhost' });
+
+module.exports = sequelize;
