3818c228a0335b02c77dfd3dce2e973d2ee67ca0
Ludo Loops <ludoloops@prontonmail.com>
2020-06-30 01:55:30 +0200

    feat 153: inserting Data and creating a product

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
-
+const sequelize = require('./util/database');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -18,5 +18,12 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
-
-app.listen(3000);
+sequelize
+  .sync()
+  .then(result => {
+    // console.log(result);
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
--- controllers/admin.js
+++ controllers/admin.js
@@ -13,14 +13,14 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null, title, imageUrl, description, price);
-  product
-    .save()
-    .then(() => {
-      res.redirect('/');
-    })
+  Product.create({
+    title: title,
+    price: price,
+    imageUrl: imageUrl,
+    description: description,
+  })
+    .then(result => console.log('created Product'))
     .catch(err => console.log(err));
-  res.redirect('/');
 };
 exports.postEditProduct = (req, res, next) => {
   const prodId = req.body.productId;
