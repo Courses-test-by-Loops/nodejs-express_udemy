bae3721363e150516c81c848291b4b5854c36f5c
Ludo Loops <ludoloops@prontonmail.com>
2020-06-30 16:38:04 +0200

    feat 163: using magic association Methods

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -11,17 +13,42 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+// const { HasMany } = require('sequelize/types');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
 sequelize
-  .sync()
-  .then(result => {
-    // console.log(result);
+  .sync({
+    // force: true,  // permet de reset la db
+  })
+  .then(() => {
+    return User.findByPk(1);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'Ludo', email: 'test@email.com' });
+    }
+    return user;
+  })
+  .then(user => {
+    console.log(user);
     app.listen(3000);
   })
   .catch(err => {
